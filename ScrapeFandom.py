3826265ace9f15b0efec87d50a52f75f74f066d2
James <1308761+JOHW85@users.noreply.github.com>
2023-04-28 14:12:26 +0800

    Update ScrapeFandom.py
    Fixed some bugs of certain pages being ignored due to wikiextractor or there being just too many pages for a Fandom page

--- ScrapeFandom.py
+++ ScrapeFandom.py
@@ -1,6 +1,35 @@
 import requests
 from bs4 import BeautifulSoup
 import argparse
+from tqdm import tqdm
+import errno, os
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc: # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else: raise
+def safe_open_w(path):
+    ''' Open "path" for writing, creating any parent directories as needed.
+    '''
+    mkdir_p(os.path.dirname(path))
+    return open(path, 'wb')
+
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("disable-infobars")
+chrome_options.add_argument('--disable-extensions')
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument('--ignore-certificate-errors')
+chrome_options.add_argument('--no-sandbox')
+
 parser = argparse.ArgumentParser()
 parser.add_argument('input_fandom', help='Fandom\'s name')
 args = parser.parse_args()
@@ -8,33 +37,63 @@ fandom_site = args.input_fandom
 #Get All Pages
 nextpage_url = "/wiki/Special:AllPages"
 AllPage = "https://"+fandom_site+".fandom.com"+nextpage_url
-listofpages = ""
+counter = 0
 while nextpage_url != "":
-    print(AllPage)
-    req = requests.get(AllPage)
-    soup = BeautifulSoup(req.content,"lxml")
-    content = soup.find("div",{"class":"mw-allpages-body"})
-    nextpage = soup.find("div",{"class":"mw-allpages-nav"})
-    listofentries = content.find_all("li")
-    for i in listofentries:
-        listofpages += i.text.replace("(redirect","") + "\n"    
-    # Gets next page from AllPages
-    if nextpage is not None:
-        nav = nextpage.findAll("a")
-        if len(nav) > 0:
-            if "Next page" in nav[-1].text:
-                nextpage_url = nav[-1]["href"]
-                AllPage = "https://"+fandom_site+".fandom.com"+nextpage_url
-            else:
-                nextpage_url = ""
-                break
-    else:
-        # Break if there's only one index page on AllPages
-        break
+    listofpages = ""
+    try:
+        req = requests.get(AllPage, allow_redirects=False)
+        if req.content != b'':
+            soup = BeautifulSoup(req.content,"lxml")
+            content = soup.find("div",{"class":"mw-allpages-body"})
+            nextpage = soup.find("div",{"class":"mw-allpages-nav"})
+        else:
+            driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+            driver.get(AllPage)
+            soup = BeautifulSoup(driver.page_source, 'html.parser')
+            content = soup.find("div",{"class":"mw-allpages-body"})
+            nextpage = soup.find("div",{"class":"mw-allpages-nav"})
+            driver.quit()            
+        if content:
+            listofentries = content.find_all("li")
+            for i in tqdm(listofentries, desc="Scraping "+AllPage):
+                listofpages += i.text.replace("(redirect","") + "\n"   
+            # Exports XML file of all the pages scraped
+            payload = {'catname':'','pages':listofpages,'curonly':'1','wpDownload':1,'wpEditToken':'+\\','title':'Special:Export'}    
+            response = requests.post("https://"+fandom_site+".fandom.com/wiki/Special:Export", data=payload)
+            data = response.content
+            # Create directory 'fandom_site' if it doesn't exist
+            with safe_open_w(f"{fandom_site}_raw/{counter}.xml") as f:
+                f.write(data)
+            counter += 1
+        else:
+            print("No content found")
+        # Gets next page from AllPages
+        if nextpage:
+            nav = nextpage.findAll("a")
+            if len(nav) > 0:
+                if "Next page" in nav[-1].text:
+                    nextpage_url = nav[-1]["href"]
+                    AllPage = "https://"+fandom_site+".fandom.com"+nextpage_url
+                else:
+                    nextpage_url = ""
+                    break
+        else:
+            # Break if there's only one index page on AllPages
+            break
+    except Exception as e:
+        print("Error", e)
+        continue
     
-# Exports XML file of all the pages scraped
-payload = {'catname':'','pages':listofpages,'curonly':'1','wpDownload':1,'wpEditToken':'+\\','title':'Special:Export'}    
-response = requests.post("https://"+fandom_site+".fandom.com/wiki/Special:Export", data=payload)
-data = response.content
-with open(fandom_site+'.xml', 'wb') as s:
-    s.write(data)
+# Combine all the MediaWiki XML files into one
+# Get all files in the directory
+# Files in present working directory
+files = os.listdir(f"{fandom_site}_raw")
+# Create a new file to write to
+with open(f"{fandom_site}.xml", "w") as outfile:
+    # Loop through all files in the directory
+    for fname in files:
+        # Open each file and read it as a MediaWiki XML file
+        with open(f"{fandom_site}_raw/{fname}", "r") as infile:
+            # Write the contents of each file to the new file
+            outfile.write(infile.read())
+            outfile.write("\n")
