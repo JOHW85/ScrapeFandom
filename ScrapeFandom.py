b656531135e02aea3414c26d77d825040d98f40e
Sam Hollings <52575338+SamHollings@users.noreply.github.com>
2023-10-13 13:52:38 +0200

    Added git ignore and rearranged the repo so it can be imported by others

--- ScrapeFandom.py
+++ ScrapeFandom.py
@@ -1,8 +1,10 @@
+import argparse
 import src.scrapefandom as scrapefandom
+
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('input_fandom', help='Fandom\'s name')
     args = parser.parse_args()
     fandom_site = args.input_fandom
-    scrape(fandom_site)
+    scrapefandom.scrape(fandom_site)
